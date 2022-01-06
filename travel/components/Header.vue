e097d6d21f24a0329c3dc6f767c8d13e1a4de46f
yji35j4212 <yji35j4212@gmail.com>
2022-01-06 18:11:29 +0800

    檔案同步

---
+++ travel/components/Header.vue
@@ -0,0 +1,19 @@
+<style scoped></style>
+
+<template>
+    <div class="header">
+        <h1 class="logo">
+            <a href="">
+                <img src="/images/logo.png" alt="">
+            </a>
+        </h1>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {}
+    },
+}
+</script>
