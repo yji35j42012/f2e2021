726f0ccd342e4912767ed3dedb06719b7ca40501
yji35j4212 <yji35j4212@gmail.com>
2022-01-14 13:40:29 +0800

    檔案同步

--- travel/page/search.vue
+++ travel/page/search.vue
@@ -10,7 +10,42 @@
         <div class="infoBox">
             <h3>熱門主題</h3>
             <ul class="r2c4">
-                <li class="r2c4_item" data-txt="自然風景類"></li>
+                <li
+                    class="r2c4_item"
+                    style="background-image:url('../images/search1.jpg')"
+                >
+                    自然風景類
+                </li>
+                <li
+                    class="r2c4_item"
+                    style="background-image:url('../images/search1.jpg')"
+                >
+                    觀光工廠類
+                </li>
+                <li
+                    class="r2c4_item"
+                    style="background-image:url('../images/search1.jpg')"
+                >
+                    觀光工廠類
+                </li>
+                <li
+                    class="r2c4_item"
+                    style="background-image:url('../images/search1.jpg')"
+                >
+                    觀光工廠類
+                </li>
+                <li
+                    class="r2c4_item"
+                    style="background-image:url('../images/search1.jpg')"
+                >
+                    觀光工廠類
+                </li>
+                <li
+                    class="r2c4_item"
+                    style="background-image:url('../images/search1.jpg')"
+                >
+                    觀光工廠類
+                </li>
             </ul>
         </div>
     </div>
