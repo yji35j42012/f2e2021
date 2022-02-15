991ddd79b553745528859ceab774bd36514a41d4
yji35j4212 <yji35j4212@gmail.com>
2022-02-15 16:45:50 +0800

    檔案同步

--- travel/components/Page.vue
+++ travel/components/Page.vue
@@ -1,52 +1,52 @@
 <style scoped></style>
 
 <template>
-    <ul class="page">
-        <li
-            :class="['page_prev', nowpage == 1 ? '_disabled' : '']"
-            @click="changePage('--')"
-        >
-            <i class="icon_arrow">
-                <svg viewBox="0 0 16 16">
-                    <path :d="icon_all.arrow" />
-                </svg>
-            </i>
-        </li>
-        <li
-            :class="['page_li', nowpage == index + 1 ? 'on' : '']"
-            v-for="(item, index) in maxpage"
-            :key="index"
-            @click="changePage(index + 1)"
-        >
-            {{ index + 1 }}
-        </li>
+	<ul class="page">
+		<li
+			:class="['page_prev', nowpage == 1 ? '_disabled' : '']"
+			@click="changePage('--')"
+		>
+			<i class="icon_arrow">
+				<svg viewBox="0 0 16 16">
+					<path :d="icon_all.arrow" />
+				</svg>
+			</i>
+		</li>
+		<li
+			:class="['page_li', nowpage == index + 1 ? 'on' : '']"
+			v-for="(item, index) in maxpage"
+			:key="index"
+			@click="changePage(index + 1)"
+		>
+			{{ index + 1 }}
+		</li>
 
-        <li
-            :class="['page_next', nowpage == maxpage ? '_disabled' : '']"
-            @click="changePage('++')"
-        >
-            <i class="icon_arrow">
-                <svg viewBox="0 0 16 16">
-                    <path :d="icon_all.arrow" />
-                </svg>
-            </i>
-        </li>
-    </ul>
+		<li
+			:class="['page_next', nowpage == maxpage ? '_disabled' : '']"
+			@click="changePage('++')"
+		>
+			<i class="icon_arrow">
+				<svg viewBox="0 0 16 16">
+					<path :d="icon_all.arrow" />
+				</svg>
+			</i>
+		</li>
+	</ul>
 </template>
 
 <script>
 module.exports = {
-    data() {
-        return {
-            icon_all: icon_all,
-        };
-    },
-    props: ["maxpage", "nowpage"],
+	data() {
+		return {
+			icon_all: icon_all,
+		};
+	},
+	props: ["maxpage", "nowpage"],
 
-    methods: {
-        changePage(str) {
-            this.$emit("changepage", str);
-        },
-    },
+	methods: {
+		changePage(str) {
+			this.$emit("changepage", str);
+		},
+	},
 };
 </script>
