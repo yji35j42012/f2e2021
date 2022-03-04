3ba74462436e2226e6c09916021ce77b1d827361
yji35j4212 <yji35j4212@gmail.com>
2022-03-04 17:01:59 +0800

    檔案同步

---
+++ travel/test/userform.vue
@@ -0,0 +1,24 @@
+<style scoped></style>
+
+<template>
+    <form id="user-form" @submit.prevent="$emit('submit')">
+        <user-profile :name="name"></user-profile>
+        <!-- <account :account="account"></account> -->
+    </form>
+</template>
+
+<script>
+module.exports = {
+    prop: {
+        name: {
+            type: "String",
+            required: true,
+        },
+        account: {
+            type: "String",
+            required: true,
+        },
+    },
+    mounted() {},
+};
+</script>
