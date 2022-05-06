dbd890b962758099ac576d9f1abac0e0d7ba112f
yji35j4212 <yji35j4212@gmail.com>
2022-05-06 18:05:02 +0800

    檔案同步

---
+++ bus/page/home.vue
@@ -0,0 +1,17 @@
+<style scoped></style>
+
+<template>
+    <div class="container">
+		home
+	</div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {};
+    },
+    mounted() {},
+    methods: {},
+};
+</script>
---
+++ bus/page/routeSearch.vue
@@ -0,0 +1,51 @@
+<style scoped></style>
+
+<template>
+    <div class="container">
+        <div class="inpBox">
+            <h2>請選擇地點並輸入編號</h2>
+            <div class="keyboard_box">
+                <button class="keyboard_btn">雙北</button>
+                <button class="keyboard_btn">桃園</button>
+                <button class="keyboard_btn">台中</button>
+                <button class="keyboard_btn">高雄</button>
+                <button class="keyboard_btn">更多</button>
+            </div>
+            <input type="text" class="keyboard_inp" />
+            <div class="keyboard_box">
+                <button class="keyboard_btn">紅</button>
+                <button class="keyboard_btn">黃</button>
+                <button class="keyboard_btn _num">７</button>
+                <button class="keyboard_btn _num">８</button>
+                <button class="keyboard_btn _num">９</button>
+                <button class="keyboard_btn">藍</button>
+                <button class="keyboard_btn">棕</button>
+                <button class="keyboard_btn _num">４</button>
+                <button class="keyboard_btn _num">５</button>
+                <button class="keyboard_btn _num">６</button>
+                <button class="keyboard_btn">綠</button>
+                <button class="keyboard_btn">橘</button>
+                <button class="keyboard_btn _num">1</button>
+                <button class="keyboard_btn _num">2</button>
+                <button class="keyboard_btn _num">3</button>
+                <button class="keyboard_btn _double">更多</button>
+                <button class="keyboard_btn">重設</button>
+                <button class="keyboard_btn _num">0</button>
+                <button class="keyboard_btn">back</button>
+            </div>
+        </div>
+        <ul class="list"></ul>
+        <div class="map"></div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {};
+    },
+    mounted() {},
+    computed: {},
+    methods: {},
+};
+</script>
