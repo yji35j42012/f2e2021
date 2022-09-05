9463c19a57586a2d3aac04c13c58514778373b79
yji35j4212 <yji35j4212@gmail.com>
2022-09-05 16:50:45 +0800

    檔案同步

--- bus/page/home.vue
+++ bus/page/home.vue
@@ -2,15 +2,30 @@
 
 <template>
     <div class="container">
-		<div class="home_txt"></div>
-        <div class="home_pic"></div>
-	</div>
+        <div class="home_serach">
+            <h1>
+                歡迎來到公車總站 <br />
+                今天要搭<span class="line" data-txt="哪班車"></span>呢？
+            </h1>
+            <button class="normal_btn _search">
+                路線搜尋
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21">
+                    <path :d="icon_all.search" />
+                </svg>
+            </button>
+        </div>
+        <div class="home_pic">
+            <img src="../images/bus.jpg" alt="" />
+        </div>
+    </div>
 </template>
 
 <script>
 module.exports = {
     data() {
-        return {};
+        return {
+            icon_all: icon_all,
+        };
     },
     mounted() {},
     methods: {},
